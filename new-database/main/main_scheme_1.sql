d8342376e5548dfc8d0432f0d9445314094260af
Glitch (glitch-hello-node) <none>
2021-05-10 17:13:22 +0000

    🛣👷 Checkpoint ./package.json:28902414/624 ./express.js:28902414/323

--- new-database/main/main_scheme_1.sql
+++ new-database/main/main_scheme_1.sql
@@ -1,1 +1,1 @@
-{}
+{"suggest_system_841144702542675968":{"key":"suggest_system_841144702542675968","value":"true"},"suggestchannel_841144702542675968":{"key":"suggestchannel_841144702542675968","value":"841145338944421898"}}
