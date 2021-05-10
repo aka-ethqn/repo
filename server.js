401984353c365da232e636b87a05bdc7010ed69b
Glitch (glitch-hello-node) <none>
2021-05-10 17:01:23 +0000

    💏❣️ Checkpoint ./server.js:28902414/12502 ./package.json:28902414/1438

--- server.js
+++ server.js
@@ -1,88 +1,106 @@
-const path = require("path");
+  const dbd = require("dbd.js")
+ 
+const bot = new dbd.Bot({
+token: "ODQxMzUyMDA4ODI5MzcwMzY4.YJlgHg.Ka1HjQ5ge68-mWBGYQEXi4vrwjI", 
+prefix: "$getServerVar[prefix]", 
+})
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // set this to true for detailed logging:
-  logger: false
-});
 
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
+bot.onMessage()
+ 
+bot.command({
+name: "ping", 
+code: `Pong! \`$ping\` ms` 
 
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
+})
 
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
-});
+bot.status({
+ 
+text: "$allMembersCount members",
+ 
+type: "WATCHING",
+ 
+time: 12
+ 
+})
+ 
+bot.command({
+name: "setprefix",
+aliases: ['changeprefix', 'prefix'],
+code: `$author[Success;https://cdn.discordapp.com/attachments/760236507310850102/780441559468474408/6286_tada_animated.gif]
+$description[**Done, my new prefix is** \`$message\`]
+$color[RANDOM]
+$setServerVar[prefix;$message]
+$onlyIf[$message[1]!=;**You have to put a prefix, example** \`$getServerVar[prefix]setprefix /\`]
+$onlyPerms[admin;$customEmoji[Rufy] **You dont have** \`ADMIN\` **perms**]`
+})
+ 
+bot.variables({
+    prefix: "!",
+  suggestchannel: "", 
+  suggest_system: "no"
+    
+})
 
-// load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
+bot.command({
+  name: "embed",
+  code:  `$channelSendMessage[$mentionedChannels[1;yes];{description:$messageSlice[1]}{image:$messageAttachment}]`
+})
 
-// Our home page route, this pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  // check and see if someone asked for a random color
-  if (request.query.randomize) {
-    // we need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
-    };
-  }
-  reply.view("/src/pages/index.hbs", params);
-});
+bot.command({
+ name: "enable-suggest",
+ code: ` 
+$setServerVar[suggest_system;true]
+Enabled! 
+$onlyPerms[manageserver;You need manage server permissions]`
+ })
 
-// A POST route to handle and react to form submissions 
-fastify.post("/", function(request, reply) {
-  let params = { seo: seo };
-  // the request.body.color is posted with a form submission
-  let color = request.body.color;
-  // if it's not empty, let's try to find the color
-  if (color) {
-    // load our color data file
-    const colors = require("./src/colors.json");
-    // take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    // now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
-      // try again.
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
-  reply.view("/src/pages/index.hbs", params);
-});
+bot.command({
+ name: "disable-suggest",
+  code:  `$channelSendMessage[$mentionedChannels[1;yes];{description:$messageSlice[1]}{image:$messageAttachment}]`
+})
 
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
+bot.command({
+ name: "enable-suggest",
+ code: ` 
+$setServerVar[suggest_system;true]
+Enabled! 
+$onlyPerms[manageserver;You need manage server permissions]`
+ })
+
+bot.command({
+ name: "disable-suggest",
+ code: `
+$setServerVar[suggest_system;no]
+Disabled!
+$onlyPerms[manageserver;You need manage server permissions]`
+ })
+
+bot.command({
+ name: "set-suggest-channel",
+ code: `
+$setServerVar[suggestchannel;$mentionedChannels[1]] 
+Set suggest channel to <#$mentionedChannels[1]> 
+$argsCheck[>1;Please mention a channel] 
+$onlyPerms[manageserver;You need manage server permission]
+$onlyIf[$getServerVar[suggest_system]==true;Suggest system is not enabled!]
+`
+ })
+
+bot.command({
+ name: "suggest",
+ code: `$useChannel[$getServerVar[suggestchannel]]
+ $title[**Suggestion!**]
+ $description[
+ **SUGGESTED BY
+ **$username
+ **Suggestion:**
+ $message ]
+ $color[YELLOW]
+ $footer[✅ I love it / ❌ I don't like it]
+ $addCmdReactions[✅]
+ $addReactions[✅;❌] 
+$onlyIf[$getServerVar[suggestchannel]!=;This server has no suggest channel run **<setsuggestchannel** to set one!]
+$onlyIf[$getServerVar[suggest_system]==true;Suggest system is not enabled!]
+ `
+ });
